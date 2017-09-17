c17536c5e2f20fd9b4c27ac8fcfdb0e863274b94
Evan Zlotnick <evan_zlotnick@yahoo.com>
2017-09-16 23:16:31 -0500

    trying without the npm install

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,17 @@
 
 echo "Hello, world!"
 
-npm install
+echo "Downloading npm modules - cheating using npm install"
+
+mkdir node_modules
+
+cd node_modules
+
+aws s3 sync s3://sandbox-npm-modules/react-boilerplate/ ./
+
+cd ..
+
+echo "Got all the modules... running npm build"
 
 npm run build
 

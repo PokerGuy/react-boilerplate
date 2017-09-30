e67118af8069545aba096528fe10e08aebec153f
Evan Zlotnick <evan_zlotnick@yahoo.com>
2017-09-30 17:35:47 -0500

    Testing a deploy to sandbox

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -19,7 +19,7 @@ function Footer() {
         <FormattedMessage
           {...messages.authorMessage}
           values={{
-            author: <A href="https://twitter.com/mxstbr">Evan Zlotnick</A>,
+            author: <A href="https://twitter.com/mxstbr">Sandbox</A>,
           }}
         />
       </section>

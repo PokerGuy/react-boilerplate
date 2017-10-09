3f0369ec3aed4db3924c1db66e5755a1556ca500
Evan Zlotnick <evan.zlotnick@bigcommerce.com>
2017-10-09 09:50:56 -0500

    made a table for the build board

--- app/components/ReposList/index.js
+++ app/components/ReposList/index.js
@@ -1,18 +1,31 @@
 import React, {PropTypes} from 'react';
+const Reactable = require('reactable');
+const Table = Reactable.Table;
+const Tr = Reactable.Tr;
+const Td = Reactable.Td;
+const _ = require('lodash');
 const moment = require('moment-timezone');
 
 function ReposList(repos) {
   let content = (<div>Fetching Repos...</div>);
   if (repos.repos) {
-    content = repos.repos.map(function (repo, index) {
-      return <div key={index} className="yellow">
-        &nbsp;<a href="#">{repo.repo_name}</a><br/>
-        &nbsp;Committer: {repo.committer.name}<br/>
-        &nbsp;Start Time: {moment.tz(repo.start_time, "America/Chicago").format('hh:mm:ss a MM/DD/YYYY')}<br/>
-        &nbsp;Commit Message: {repo.message}<br/>
-        &nbsp;Hash: {repo.hash.substring(0, 4)}
-      </div>
-    });
+    content = <Table className="table" id="table" itemsPerPage={4} pageButtonLimit={5} sortable={['Repo', 'Committer', 'Start Time']} filterable={['Repo', 'Committer', 'Start Time', 'Hash']}>
+    {repos.repos.map(function(repo, index) {
+      let status="green";
+      console.log(repo.end_time);
+      if (repo.error || (repo.start_time < ((new Date).getTime() - (5 * 60 * 1000)) && repo.end_time === undefined)) {
+        status = "red";
+      } else if (repo.end_time === undefined && repo.start_time > ((new Date).getTime() - (5 * 60 * 1000))) {
+        status = "yellow";
+      }
+      return <Tr key={index}>
+        <Td column="Repo" className={status}>{repo.repo_name}</Td>
+        <Td column="Committer" className={status}>{repo.committer.name}</Td>
+        <Td column="Start Time" className={status}>{moment.tz(repo.start_time, "America/Chicago").format('hh:mm:ss a MM/DD/YYYY')}</Td>
+        <Td column="Hash" className={status}>{repo.hash.substring(0,4)}</Td>
+      </Tr>
+    })}
+    </Table>
   }
   return <div>
     {content}

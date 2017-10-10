d5f768b4ebcd24d6d7f27c21ccc48002fa37a8eb
Evan Zlotnick <evan.zlotnick@bigcommerce.com>
2017-10-10 15:13:44 -0500

    finally an awesome header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Logo from './synergy.png';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div>
-        Awesome Header
+        <img src={Logo} /> Serverless DevOps the Easy Way
       </div>
     );
   }

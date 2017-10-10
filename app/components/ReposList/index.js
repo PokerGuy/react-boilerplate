b07186781ab0cad969217d7364c1366e04220a12
Evan Zlotnick <evan.zlotnick@bigcommerce.com>
2017-10-10 13:47:20 -0500

    more formatting

--- app/components/ReposList/index.js
+++ app/components/ReposList/index.js
@@ -5,6 +5,20 @@ import 'react-table/react-table.css';
 const _ = require('lodash');
 const moment = require('moment-timezone');
 //https://react-table.js.org/#/story/cell-renderers-custom-components
+function formatTime(time) {
+  return moment.tz(time, "America/Chicago").format('MM/DD/YYYY hh:mm:ss a');
+}
+
+
+function cellColor(row) {
+  if (row.original.error || (row.original.start_time < ((new Date).getTime() - (5 * 60 * 1000)) && row.original.end_time === undefined)) {
+    return "red";
+  } else if (row.original.end_time === undefined && row.original.start_time > ((new Date).getTime() - (5 * 60 * 1000))) {
+    return  "yellow";
+  }
+  return "green";
+}
+
 function ReposList(repos) {
   let content = (<div>Fetching Repos...</div>);
   if (repos.repos) {
@@ -13,20 +27,44 @@ function ReposList(repos) {
       Header: 'Repo',
       accessor: 'repo_name',
       Cell: row => (
-        <div
-        style={{backgroundColor: '#85cc00'}}>
-    {row.value}
-    </div>
+        <span>
+          <span style={{
+            color: cellColor(row),
+            transition: 'all .3s ease'
+    }}> &#x25cf;
+        </span>{row.value}</span>
       )
     },
     {
       Header: 'Committer',
       accessor: 'committer.name'
     },
+      {
+        Header: 'Start Time',
+        accessor: 'start_time',
+        Cell: row => (
+          formatTime(row.value)
+        )
+      },
     {
       Header: 'Message',
       accessor: 'message'
-    }];
+    },
+      {
+        Header: 'Git Hash',
+        accessor: 'hash',
+        Cell: row => (
+          row.value.substring(0,6)
+        )
+      },
+      {
+        Header: 'Builds',
+        accessor: 'repo_name',
+        Cell: row => (
+          <Link to={'/build/' + row.value}>Details</Link>
+        )
+      }
+      ];
     content = <ReactTable data={repos.repos} columns={columns}/>
   }
   return <div>
@@ -39,3 +77,11 @@ ReposList.propTypes = {
 };
 
 export default ReposList;
+
+/*
+if (repo.error || (repo.start_time < ((new Date).getTime() - (5 * 60 * 1000)) && repo.end_time === undefined)) {
+        status = "red";
+      } else if (repo.end_time === undefined && repo.start_time > ((new Date).getTime() - (5 * 60 * 1000))) {
+        status = "yellow";
+      }
+ */
